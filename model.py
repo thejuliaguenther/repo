e5f85d40c8e89c834074907e7347ca5f0236aeb1
Julia Guenther <juliag1@alumni.stanford.edu>
2015-10-08 18:09:04 -0700

    TEST

---
+++ model.py
@@ -0,0 +1,56 @@
+employee_directory = {
+    "joel": {
+        "name": "Joel Burton",
+        "email": "joel@hackbrightacademy.com",
+        "favorite_food": "peanut-butter pretzels",
+        "position": "Director of Fellowship Education"
+    },
+    "cynthia": {
+        "name": "Cynthia Dueltgen",
+        "email": "cynthia@hackbrightacademy.com",
+        "favorite_food": "mint shake",
+        "position": "Instructor"
+    },
+    "rachel": {
+        "name": "Rachel Thomas",
+        "email": "rthomas@hackbrightacademy.com",
+        "favorite_food": "dried mango",
+        "position": "Instructor"
+    },
+    "meggie": {
+        "name": "Meggie Mahnken",
+        "email": "meggie@hackbrightacademy.com",
+        "favorite_food": "Foods with single, overpowering flavors",
+        "position": "TA"
+    },
+    "kristen": {
+        "name": "Kristen McClure",
+        "email": "kmcclure@hackbrightacademy.com",
+        "favorite_food": "hamburgers",
+        "position": "TA"
+    },
+    "heather": {
+        "name": "Heather Bryant",
+        "email": "heather@hackbrightacademy.com",
+        "favorite_food": "Jell-O",
+        "position": "TA"
+    },
+    "katie": {
+        "name": "Katie Lefevre",
+        "email": "katie@hackbrightacademy.com",
+        "favorite_food": "Kale",
+        "position": "TA"
+    },
+    "lindsay": {
+        "name": "The Wizard",
+        "email": "unknown",
+        "favorite_food": "unknown",
+        "position": "wizard"
+    },
+    "mel": {
+        "name": "Mel",
+        "email": "mel@ubermelon.com",
+        "favorite_food": "Powerbars",
+        "position": "The Boss"
+    }
+}
